cbe97b4ee247cb0d52de927f7f348febb9bd01a3
Taehee Lim <lim3617@naver.com>
2022-01-20 12:45:20 +0900

    commit^^

--- public/js/app.js
+++ public/js/app.js
@@ -17,7 +17,7 @@ function addMessage(message){
 
 function handleMessageSubmit(event){
     event.preventDefault();
-    const input = room.querySelector('input')
+    const input = room.querySelector('#msg input')
     const value = input.value
     socket.emit("new_message", input.value, roomName, () => {
         addMessage(`You : ${value}`);
@@ -25,13 +25,21 @@ function handleMessageSubmit(event){
     input.value = ''
 }
 
+function handleNicknameSubmit(event){
+    event.preventDefault();
+    const input = room.querySelector('#name input')
+    socket.emit('nickname', input.value)
+}
+
 function showRoom(){
     welcome.hidden = true
     room.hidden = false
     const h3 = room.querySelector('h3');
     h3.innerText = `Room ${roomName}`
-    const form = room.querySelector("form")
-    form.addEventListener("submit", handleMessageSubmit)
+    const msgForm = room.querySelector("#msg")
+    const nameForm = room.querySelector("#name")
+    msgForm.addEventListener("submit", handleMessageSubmit)
+    nameForm.addEventListener("submit", handleNicknameSubmit)
 }
 
 //서버에 socket emit해서 방 이름 전송
@@ -50,15 +58,32 @@ function handleRoomSubmit(event){
 
 form.addEventListener("submit", handleRoomSubmit);
 
-socket.on("welcome", () => {
-    addMessage("someone joined!!")
+socket.on("welcome", (user, newCount) => {
+    const h3 = room.querySelector('h3');
+    h3.innerText = `Room ${roomName} (${newCount})`
+    addMessage(`${user} arrived`)
 }) 
 
-socket.on("bye", () => {
-    addMessage("someone left!!")
+socket.on("bye", (left, newCount) => {
+    const h3 = room.querySelector('h3');
+    h3.innerText = `Room ${roomName} (${newCount})`
+    addMessage(`${left} left`)
 })
 
 socket.on("new_message", addMessage)
+
+socket.on("room_change", (rooms) => {
+    const roomList = welcome.querySelector('ul')
+    if(rooms.length === 0 ){
+        roomList.innerHTML = ''
+        return
+    }
+    rooms.forEach(room => {
+        const li = document.createElement('li')
+        li.innerText = room
+        roomList.append(li)
+    })
+})
 
 
 
--- server.js
+++ server.js
@@ -15,7 +15,29 @@ app.get("/*", (req, res) => res.redirect("/"));
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
+// private room 말고 public room 구함
+function publicRooms(){
+    const {
+        sockets : {
+            adapter : {sids, rooms},
+        },
+    } = wsServer
+    const publicRooms = []
+    rooms.forEach((_, key) => {
+        if(sids.get(key) === undefined){
+            publicRooms.push(key)
+        }
+    })
+    return publicRooms
+}
+
+function countRoom(roomName){
+    return wsServer.sockets.adapter.rooms.get(roomName)?.size;
+    
+}
+
 wsServer.on("connection", socket => {
+    socket["nickname"] = "Anonymous"
     socket.onAny((event) => {
         console.log(`socket event : ${event}`)
     })
@@ -23,15 +45,22 @@ wsServer.on("connection", socket => {
         //채팅방 입장
         socket.join(roomName);
         done();
-        socket.to(roomName).emit("welcome");
+        socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
+        wsServer.sockets.emit("room_change", publicRooms())
     });
+    //소켓연결이 끊기기 직전에
     socket.on("disconnecting", () => {
-        socket.rooms.forEach(room => socket.to(room).emit("bye"))
+        socket.rooms.forEach(room => socket.to(room).emit("bye", socket.nickname, countRoom(room) -1))
+    })
+    //소켓연결이 끊기고나서
+    socket.on("disconnect", () => {
+        wsServer.sockets.emit("room_change", publicRooms())
     })
     socket.on("new_message", (msg, room, done) => {
-        socket.to(room).emit("new_message", msg);
+        socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
         done();
     })
+    socket.on("nickname", nickname => (socket["nickname"] = nickname))
 })
 
 function onSocketClose(){
